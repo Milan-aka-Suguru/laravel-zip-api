21999818758b646accb5e2787c8f257a607ce945
KissKolos <113348700+KissKolos@users.noreply.github.com>
2025-11-03 08:05:54 +0100

    Végső api (2025.11.03)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    //"php artisan db:seed" és "php artisan db:seed --class:TownsFromCsvSeeder" -t kell használni, mivel valamiért a towns feltöltés nem akar működni ¯\_(ツ)_/¯
     public function run(): void
     {
         User::factory() ->create([
@@ -18,6 +19,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
             'password' => 'asd',
         ]);
-        
+
     }
 }
